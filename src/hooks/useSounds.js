20c586d10551128bacf14ea2f7d72f951c8480fd
evfinkn <evan@finken.name>
2023-04-23 16:02:02 -0500

    Redo useEffect so multiple event listeners aren't added

--- src/hooks/useSounds.js
+++ src/hooks/useSounds.js
@@ -48,10 +48,23 @@ export default (urls) => {
 
   // handles updating the audio's "playing" property after the audio has ended
   const handleEnded = (event) => sounds[event.target.src].toggle();
-  Object.values(sounds).forEach((sound) => {
-    // when an audio clip ends, it needs to update "playing"
-    sound.audio.addEventListener("ended", handleEnded);
-  });
+
+  // use useEffect so that adding the event listener isn't
+  // run everytime a sound's properties are updated
+  useEffect(() => {
+    Object.values(sounds).forEach((sound) => {
+      // when an audio clip ends, it needs to update "playing"
+      sound.audio.addEventListener("ended", handleEnded);
+    });
+    // returning a function makes that function get called on unmount
+    return () => {
+      // cleans up by removing event listeners so that
+      // the audio elements can be garbage collected
+      Object.values(sounds).forEach((sound) => {
+        sound.audio.removeEventListener("ended", handleEnded);
+      });
+    }; // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // [] makes this run only on mount and unmount (and not on update)
 
   return sounds;
 };
